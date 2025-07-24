a63cd1dd63fa19876f67464a5a582a2f9d2183e2
Ata <eng.ata2003@gmail.com>
2025-07-24 15:00:11 +0330

    Add test.js

---
+++ test.js
@@ -0,0 +1,19 @@
+ const list = ["elyar", "ata", "sina", "milad"]
+var random = Math.floor(list.length * Math.random())
+ list.splice(random , 1)
+ console.log(list)
+
+var random = Math.floor(list.length * Math.random())
+ list.splice(random , 1)
+ console.log(list)
+
+var random = Math.floor(list.length * Math.random())
+ list.splice(random , 1)
+ console.log(list)
+
+var random = Math.floor(list.length * Math.random())
+ list.splice(random , 1)
+ console.log(list)
+
+
+
